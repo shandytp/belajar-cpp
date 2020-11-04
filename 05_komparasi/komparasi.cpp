b82b75fbe9e92846ea5f16dda1f6d932e6a6e3ce
shandytp <shandytsalasa@gmail.com>
2020-11-04 23:59:47 +0700

    04-10-2020

--- 05_komparasi/komparasi.cpp
+++ 05_komparasi/komparasi.cpp
@@ -23,13 +23,13 @@ int main()
 	hasil5 = (a <= b);
 	// lebih dari sama dengan (>=)
 	hasil6 = (a >= b);
-
-	cout << hasil1 << endl;
-	cout << hasil2 << endl;
-	cout << hasil3 << endl;
-	cout << hasil4 << endl;
-	cout << hasil5 << endl;
-	cout << hasil6 << endl;
+	cout << "\n";
+	cout << "Hasil dari logika sebanding : " << hasil1 << endl;
+	cout << "Hasil dari logika tidak sebanding : " << hasil2 << endl;
+	cout << "Hasil dari logika kurang dari : " << hasil3 << endl;
+	cout << "Hasil dari logika lebih dari : " <<  hasil4 << endl;
+	cout << "Hasil dari logika kurang dari sama dengan : " <<  hasil5 << endl;
+	cout << "Hasil dari logika lebih dari sama dengan : " <<  hasil6 << endl;
 
 	cin.get();
 	return 0;
